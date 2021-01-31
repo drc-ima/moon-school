dd04df24c76a0cf3f53cf7b7a903a0afc6228924
drc.alex <emmanuelofosu472@gmail.com>
2021-01-31 22:39:07 +0000

    removing secret key

--- school_back/settings.py
+++ school_back/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+j$oyx_g%j)nz@&$^u9i)_nb90pp!03dxvw^gkd(p0qgxls4qx'
+SECRET_KEY = 'changeme'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
