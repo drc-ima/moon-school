cf3dfe49cb13d16f6ca36886fa4476691ce07734
drc.alex <emmanuelofosu472@gmail.com>
2021-02-01 01:13:37 +0000

    icon in main

--- school_back/settings.py
+++ school_back/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'changeme'
+SECRET_KEY = '+j$oyx_g%j)nz@&$^u9i)_nb90pp!03dxvw^gkd(p0qgxls4qx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
