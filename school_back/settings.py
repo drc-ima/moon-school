aa49a3a8f6d463b7ced8d8b1515498d16481d83c
drc.alex <emmanuelofosu472@gmail.com>
2021-02-01 02:21:02 +0000

    error with whitenoise fixed

--- school_back/settings.py
+++ school_back/settings.py
@@ -35,8 +35,8 @@ ALLOWED_HOSTS = ["*"]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
-    'django.contrib.contenttypes',
     'django.contrib.humanize',
+    'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
